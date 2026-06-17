fd2b143c88fd0e2cac5e99a89bf4ddc9bfa0481f
FA-50 <wjdtn747@naver.com>
2026-06-17 09:10:20 +0900

    cicd

---
+++ src/main/java/io/rapa/practicecicd/controller/HiController.java
@@ -0,0 +1,12 @@
+package io.rapa.practicecicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+    @GetMapping("/hi")
+    public String sayHi(){
+        return "hi";
+    }
+}
