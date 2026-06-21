c28eca46cb6706ab0a7b23c25fda74dbbf936051
FA-50 <wjdtn747@naver.com>
2026-06-21 20:40:43 +0900

    cicd

--- src/test/java/io/rapa/practicecicd/controllertes.java
+++ src/test/java/io/rapa/practicecicd/controllertes.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class controllertes {
-    @Test
-    void 무조건_실패(){
-        Assertions.assertNotNull(null);
-    }
+//    @Test
+//    void 무조건_실패(){
+//        Assertions.assertNotNull(null);
+//    }
 }
